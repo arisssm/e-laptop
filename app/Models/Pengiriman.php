5d0be568cdb8541ebab1eac572cd6dca20245f58
arisssm <arisssmulianto@gmail.com>
2023-02-08 11:34:04 +0700

    create models

---
+++ app/Models/Pengiriman.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pengiriman extends Model
+{
+    use HasFactory;
+    protected $table = 'pengiriman';
+    protected $guarded = [''];
+
+    public function pesanan()
+    {
+        return $this->hasMany(Pesanan::class, 'pesanan_id', 'id');
+    }    
+}
---
+++ app/Models/Pesanan.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pesanan extends Model
+{
+    use HasFactory;
+    protected $table = 'pesanan';
+    protected $guarded = [''];
+
+    public function detail_pesanan()
+    {
+        return $this->hasMany(DetailPesanan::class, 'pesanan_id', 'id');
+    }
+    public function bank()
+    {
+        return $this->belongsTo(Bank::class, 'bank_id', 'id');
+    }
+
+    public function pengiriman()
+    {
+        return $this->belongsTo(Pengiriman::class, 'pengiriman_id', 'id');
+    }
+
+}
