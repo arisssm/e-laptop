89922d81dd69aca11c1fa16201430c56c0e1e279
arisssm <arisssmulianto@gmail.com>
2023-03-04 12:44:06 +0700

    fix bug route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\MerekController;
+use App\Http\Controllers\ProdukController;
+use App\Http\Controllers\BankController;
+use App\Http\Controllers\PengirimanController;
+use App\Http\Controllers\BannerController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +19,32 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('app');
 });
+
+// Merek
+// Route::get('/merek', [MerekController::class, 'index']);
+// Route::get('/merek/create', [MerekController::class, 'create']);
+// Route::post('/merek', [MerekController::class, 'store']);
+// Route::get('/merek/{merek}/edit', [MerekController::class, 'edit']);
+// Route::put('/merek/{merek}', [MerekController::class, 'update']);
+// Route::delete('/merek/{merek}', [MerekController::class, 'destroy']);
+
+// Banner
+// Route::get('/banner/createdua', [BannerController::class, 'createdua']);
+// Route::post('/banner/storedua', [BannerController::class, 'storedua']);
+// Route::get('/banner/{banner}/editdua', [BannerController::class, 'editdua']);
+// Route::put('/banner/{banner}/updatedua', [BannerController::class, 'updatedua']);
+// Route::delete('/banner/{banner}/destroydua', [BannerController::class, 'destroydua']);
+
+// Resource
+Route::resource('/merek', MerekController::class);
+Route::resource('/produk', ProdukController::class);
+Route::resource('/bank', BankController::class);
+Route::resource('/pengiriman', PengirimanController::class);
+Route::get('/banner/{spanduk}', [BannerController::class, 'index']);
+Route::get('/banner/create/{spanduk}', [BannerController::class, 'create']);
+Route::post('/banner/{spanduk}', [BannerController::class, 'store']);
+Route::get('/banner/edit/{id}/{spanduk}', [BannerController::class, 'edit']);
+Route::put('/banner/{id}/{spanduk}', [BannerController::class, 'update']);
+Route::delete('/banner/{id}/{spanduk}', [BannerController::class, 'destroy']);
