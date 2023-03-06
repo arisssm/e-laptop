8c2075fc9025288bcab56f60fe10c51c10ece094
arisssm <arisssmulianto@gmail.com>
2023-03-06 08:55:37 +0700

    fix master data submenu

--- resources/views/component/sidebar.blade.php
+++ resources/views/component/sidebar.blade.php
@@ -3,7 +3,7 @@
         <div class="sidebar-header">
             <div class="d-flex justify-content-between">
                 <div class="logo">
-                    <a href="{{ url('/dashboard')}}"><img src="{{ asset('assets/images/logo/logo.png') }}" alt="Logo"
+                    <a href="{{ url('/dashboard') }}"><img src="{{ asset('assets/images/logo/logo.png') }}" alt="Logo"
                             srcset=""></a>
                 </div>
                 <div class="toggler">
@@ -20,48 +20,54 @@
                     </a>
                 </li>
                 <li class="sidebar-item">
-                    <a href="{{ url('/keranjang')}}" class='sidebar-link'>
+                    <a href="{{ url('/keranjang') }}" class='sidebar-link'>
                         <i class="fa-solid fa-shopping-cart"></i>
                         <span>Keranjang</span>
                     </a>
                 </li>
                 <li class="sidebar-item">
-                    <a href="{{ url('/pesanan')}}" class='sidebar-link'>
+                    <a href="{{ url('/pesanan') }}" class='sidebar-link'>
                         <i class="fa-solid fa-shopping-bag"></i>
                         <span>Pesanan</span>
                     </a>
                 </li>
-                <li class="sidebar-title"><i class="fa-solid fa-database me-3"></i><b>Master Data</b></li>
-
-                <li class="sidebar-item @if(Request::segment(1) == 'merek') active @endif">
-                    <a href="{{ url('/merek')}}" class='sidebar-link'>
-                        <i class="fa-solid fa-tags"></i>
-                        <span>Merek</span>
+                <li class="sidebar-item  has-sub">
+                    <a href="#" class='sidebar-link'>
+                        <i class="fa-solid fa-database"></i>
+                        <span><b>Master Data</b></span>
                     </a>
-                </li>
-                <li class="sidebar-item @if(Request::segment(1) == 'produk') active @endif">
-                    <a href="{{ url('/produk')}}" class='sidebar-link'>
-                        <i class="fa-solid fa-laptop-code"></i>
-                        <span>Produk</span>         
-                    </a>
-                </li>
-                <li class="sidebar-item @if(Request::segment(1) == 'bank') active @endif">
-                    <a href="{{ url('/bank')}}" class='sidebar-link' target=''>
-                        <i class="fa-solid fa-coins"></i>
-                        <span>Bank</span>
-                    </a>
-                </li>
-                <li class="sidebar-item @if(Request::segment(1) == 'pengiriman') active @endif">
-                    <a href="{{ url('/pengiriman')}}" class='sidebar-link'>
-                        <i class="fa-solid fa-truck-monster"></i>
-                        <span>Pengiriman</span>
-                    </a>
-                </li>
-                <li class="sidebar-item @if(Request::segment(1) == 'banner') active @endif">
-                    <a href="{{ url('/banner/satu')}}" class='sidebar-link'>
-                        <i class="fa-solid fa-images"></i>
-                        <span>Banner</span>
-                    </a>
+                    <ul class="submenu ">
+                        <li class="submenu-item @if (Request::segment(1) == 'merek') active @endif">
+                            <a href="{{ url('/merek') }}">
+                                <i class="fa-solid fa-tags"></i>
+                                <span>Merek</span>
+                            </a>
+                        </li>
+                        <li class="submenu-item @if (Request::segment(1) == 'produk') active @endif">
+                            <a href="{{ url('/produk') }}">
+                                <i class="fa-solid fa-laptop-code"></i>
+                                <span>Produk</span>
+                            </a>
+                        </li>
+                        <li class="submenu-item @if (Request::segment(1) == 'bank') active @endif">
+                            <a href="{{ url('/bank') }}">
+                                <i class="fa-solid fa-coins"></i>
+                                <span>Bank</span>
+                            </a>
+                        </li>
+                        <li class="submenu-item @if (Request::segment(1) == 'pengiriman') active @endif">
+                            <a href="{{ url('/pengiriman') }}">
+                                <i class="fa-solid fa-truck-monster"></i>
+                                <span>Pengiriman</span>
+                            </a>
+                        </li>
+                        <li class="submenu-item @if (Request::segment(1) == 'banner') active @endif">
+                            <a href="{{ url('/banner/satu') }}">
+                                <i class="fa-solid fa-images"></i>
+                                <span>Banner</span>
+                            </a>
+                        </li>
+                    </ul>
                 </li>
             </ul>
         </div>
